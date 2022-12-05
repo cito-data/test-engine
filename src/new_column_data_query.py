ec70aa237bc3db9ae45627cb8f6dce72e156d034
felix-brockmeier <felix.brockmeier@outlook.com>
2022-12-05 21:54:52 +0100

    updated queries

--- src/new_column_data_query.py
+++ src/new_column_data_query.py
@@ -1,33 +1,33 @@
 def getDistributionQuery(dbName: str, schemaName: str, materializationName: str, columnName: str):
-    return f"""select median({columnName}) as median from {dbName}.{schemaName}.{materializationName};"""
+    return f"""select median("{columnName}") as median from "{dbName}"."{schemaName}"."{materializationName}";"""
 
 def getCardinalityQuery(dbName: str, schemaName: str, materializationName: str, columnName: str):
     return f"""
-    select count(distinct({columnName})) as distinct_value_count 
-    from {dbName}.{schemaName}.{materializationName};
+    select count(distinct("{columnName}")) as distinct_value_count 
+    from "{dbName}"."{schemaName}"."{materializationName}";
   """
 
 def getUniquenessQuery(dbName: str, schemaName: str, materializationName: str, columnName: str):
     return f"""
-    select count(distinct({columnName})) as distinct_value_count,
-    count({columnName}) as non_null_value_count,
+    select count(distinct("{columnName}")) as distinct_value_count,
+    count("{columnName}") as non_null_value_count,
     distinct_value_count/non_null_value_count as uniqueness_rate
-    from {dbName}.{schemaName}.{materializationName};
+    from "{dbName}"."{schemaName}"."{materializationName}";
   """
 
 def getNullnessQuery(dbName: str, schemaName: str, materializationName: str, columnName: str):
     return f"""
-    select sum(case when {columnName} is null then 1 else 0 end) as null_value_count,
-    count({columnName}) as non_null_value_count,
+    select sum(case when "{columnName}" is null then 1 else 0 end) as null_value_count,
+    count("{columnName}") as non_null_value_count,
     null_value_count/(null_value_count + non_null_value_count) as nullness_rate 
-    from {dbName}.{schemaName}.{materializationName};
+    from "{dbName}"."{schemaName}"."{materializationName}";
   """
 
 def getFreshnessQuery(dbName: str, schemaName: str, materializationName: str, columnName: str):
   return f"""  select
   sysdate() as now, 
-  datediff(minute, {columnName}, now) as time_diff
-  from {dbName}.{schemaName}.{materializationName}
-  order by {columnName} desc nulls last
+  datediff(minute, "{columnName}", now) as time_diff
+  from "{dbName}"."{schemaName}"."{materializationName}"
+  order by "{columnName}" desc nulls last
   limit 1;
   """
--- src/new_materialization_data_query.py
+++ src/new_materialization_data_query.py
@@ -6,29 +6,29 @@ class MaterializationType(Enum):
 def getRowCountQuery(dbName: str, schemaName: str, materializationName: str, materializationType: MaterializationType):
   
   if materializationType == MaterializationType.Table:
-    return f"""select row_count from {dbName}.information_schema.tables 
-    where table_schema = '{schemaName.upper()}' and table_name = '{materializationName.upper()}' limit 1;
+    return f"""select row_count from "{dbName}".information_schema.tables 
+    where table_schema = '{schemaName}' and table_name = '{materializationName}' limit 1;
     """
 
-  return f"""select count(*) as row_count from {dbName}.{schemaName}.{materializationName};
+  return f"""select count(*) as row_count from "{dbName}"."{schemaName}"."{materializationName}";
   """
 
 def getColumnCountQuery(dbName: str, schemaName: str, materializationName: str):
-  return f"""select count(column_name) as column_count from {dbName}.information_schema.columns
-  where table_schema = '{schemaName.upper()}' and table_name = '{materializationName.upper()}';
+  return f"""select count(column_name) as column_count from "{dbName}".information_schema.columns
+  where table_schema = '{schemaName}' and table_name = '{materializationName}';
   """
 
 def getFreshnessQuery(dbName: str, schemaName: str, materializationName: str, materializationType: MaterializationType):
   return f"""select convert_timezone('UTC', last_altered)::timestamp as last_altered_converted, 
   sysdate() as now, 
   datediff(minute, last_altered_converted, now) as time_diff
-  from {dbName}.information_schema.{materializationType}s
-  where table_schema = '{schemaName.upper()}' and table_name = '{materializationName.upper()}' limit 1;"""
+  from "{dbName}".information_schema.{materializationType}s
+  where table_schema = '{schemaName}' and table_name = '{materializationName}' limit 1;"""
 
 def getSchemaChangeQuery(dbName: str, schemaName: str, tableName: str):
   return f"""with
-  schema_cte as (select column_name, data_type, is_identity, is_nullable, ordinal_position from {dbName}.information_schema.columns 
-  where table_catalog = '{dbName.upper()}' and table_schema = '{schemaName.upper()}' and table_name = '{tableName.upper()}'
+  schema_cte as (select column_name, data_type, is_identity, is_nullable, ordinal_position from "{dbName}".information_schema.columns 
+  where table_catalog = '{dbName}' and table_schema = '{schemaName}' and table_name = '{tableName}'
   order by ordinal_position)
   select object_construct(*) as column_definition from schema_cte
   """
