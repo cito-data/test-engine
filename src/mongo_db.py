46a64cb2395875e6f965a3cfe7ae08f8d73edf08
aah120 <aah120@ic.ac.uk>
2023-04-28 16:16:49 +0100

    small changes regarding environment variables

--- src/mongo_db.py
+++ src/mongo_db.py
@@ -5,4 +5,8 @@ def get_mongo_connection():
     details = getMongoDetails()
     client = MongoClient(details[1])
     print("Successfully connected to MongoDb")
+
+    if not details[0]:
+        raise Exception("Cannot have undefined database name")
+    
     return client[details[0]]
